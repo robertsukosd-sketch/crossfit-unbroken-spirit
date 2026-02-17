af89f089ce426d00684bac3088650713bc1dd735
Robert Sukosd <robert.sukosd@gmail.com>
2026-02-17 18:33:39 +0000

    File changes

--- src/components/landing/ContactSection.jsx
+++ src/components/landing/ContactSection.jsx
@@ -131,7 +131,7 @@ export default function ContactSection() {
             {/* Map placeholder */}
             <div className="rounded-xl overflow-hidden h-64 bg-zinc-900 border border-zinc-800">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d182451.5!2d26.0!3d44.4!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1ff26958976c3%3A0x84ef4f92a804b194!2sBucure%C8%99ti!5e0!3m2!1sro!2sro!4v1699999999999!5m2!1sro!2sro"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2833.8341894651926!2d26.100449!3d44.394874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40b1ff3e1f1f1f1f%3A0x1f1f1f1f1f1f1f1f!2sSplaiul%20Unirii%20257-259%2C%20Bucuresti!5e0!3m2!1sro!2sro!4v1699999999999!5m2!1sro!2sro"
                 width="100%"
                 height="100%"
                 style={{ border: 0, filter: 'grayscale(100%) invert(92%) contrast(83%)' }}
