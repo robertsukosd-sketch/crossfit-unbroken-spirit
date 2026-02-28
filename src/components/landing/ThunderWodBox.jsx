097ddb249bc6aa516f8a104abf96265bca543492
Robert Sukosd <robert.sukosd@gmail.com>
2026-02-28 22:17:57 +0000

    File changes

--- src/components/landing/ThunderWodBox.jsx
+++ src/components/landing/ThunderWodBox.jsx
@@ -20,7 +20,7 @@ export default function ThunderWodBox({ className = '' }) {
           <img
             src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69948c0d6b8aa61f49f0a23d/c1355e37d_Mobile-Mockup.png"
             alt="ThunderWOD"
-            className="w-36 h-28 object-contain"
+            className="w-44 h-full object-contain"
           />
         </div>
 
