c378123b0c75a7260d3d5619dec027eb8b13f888
Robert Sukosd <robert.sukosd@gmail.com>
2026-03-01 09:35:52 +0000

    File changes

--- src/components/landing/ThunderWodBox.jsx
+++ src/components/landing/ThunderWodBox.jsx
@@ -16,7 +16,7 @@ export default function ThunderWodBox({ className = '' }) {
         {/* Logo */}
         <div className="flex items-center justify-center">
           <img
-            src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69948c0d6b8aa61f49f0a23d/c1355e37d_Mobile-Mockup.png"
+            src="https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/69948c0d6b8aa61f49f0a23d/892734e42_logothunderwod.jpeg"
             alt="ThunderWOD"
             className="w-36 h-28 object-contain"
           />
