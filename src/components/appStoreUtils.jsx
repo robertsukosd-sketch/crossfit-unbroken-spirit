e8755539f557df903efcbd4546818d4da82aef7c
Robert Sukosd <robert.sukosd@gmail.com>
2026-03-01 10:03:40 +0000

    File changes

--- src/components/appStoreUtils.jsx
+++ src/components/appStoreUtils.jsx
@@ -1,5 +1,11 @@
 export const IOS_URL = 'https://apps.apple.com/app/thunderwod/id1607744328';
 export const ANDROID_URL = 'https://play.google.com/store/apps/details?id=imok.thunderwod.app';
+export const DESKTOP_URL = 'https://app.thunderwod.com/#/wod';
+
+export function isDesktop() {
+  const ua = navigator.userAgent || '';
+  return !/android|iphone|ipad|ipod/i.test(ua);
+}
 
 export function getAppStoreUrl() {
   const ua = navigator.userAgent || '';
@@ -8,6 +14,17 @@ export function getAppStoreUrl() {
   return IOS_URL;
 }
 
+export function getMobileOrDesktopUrl() {
+  const ua = navigator.userAgent || '';
+  if (/android/i.test(ua)) return ANDROID_URL;
+  if (/iphone|ipad|ipod/i.test(ua)) return IOS_URL;
+  return DESKTOP_URL;
+}
+
 export function openAppStore() {
   window.open(getAppStoreUrl(), '_blank');
+}
+
+export function openMobileOrDesktop() {
+  window.open(getMobileOrDesktopUrl(), '_blank');
 }
