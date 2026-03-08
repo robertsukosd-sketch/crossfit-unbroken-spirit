4581cc3b76b152c9617ec139dcc582665d7cf054
Robert Sukosd <robert.sukosd@gmail.com>
2026-03-08 07:30:52 +0000

    File changes

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,3 +1,4 @@
+
 export const CONTACT_EMAIL = 'train@unbrokenspirit.ro';
 export const PHONE_1 = '+40 748 838 767';
 export const PHONE_2 = '+40 740 269 769';
@@ -5,6 +6,7 @@ export const GYM_ADDRESS = 'Splaiul Unirii 257-259, Sector 3, București';
 
 export const QUICK_FOOTER_LINKS = [
   { label: 'home', href: '#hero' },
+  { label: 'startHere', href: '#starthere' },
   { label: 'programs', href: '#programs' },
   { label: 'pricing', href: '#pricing' },
   { label: 'schedule', href: '#schedule' },
