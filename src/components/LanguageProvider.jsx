070f11e4218b673f290964cea5569f10b22df87c
Robert Sukosd <robert.sukosd@gmail.com>
2026-03-11 13:40:06 +0000

    File changes

--- src/components/LanguageProvider.jsx
+++ src/components/LanguageProvider.jsx
@@ -130,7 +130,7 @@ export const translations = {
     hours: "L-V: 07:00 - 20:30",
     hoursSaturday: "S: 09:00 - 11:30",
     hoursSunday: "D: Închis",
-    bookFreeClass: "Rezervā o Clasā Gratuitā",
+    bookFreeClass: "Rezervă o Clasă Gratuită",
      thunderwod: "ThunderWOD App",
     faq: "FAQ",
     quickLinks: "Link-uri Rapide",
